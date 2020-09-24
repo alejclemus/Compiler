11dbdf281ddac16afb6316ed668cb71d584fc83d
Ale <alejdelcomp@gmail.com>
2020-09-23 22:14:12 -0600

    FINISHED CLI

--- class/compiler/Compiler.java
+++ class/compiler/Compiler.java
@@ -1,52 +1,95 @@
-package compiler;
+import scanner.DScanner;
 
 import java.util.Scanner;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
 
 public class Compiler {
+    public String filename;
     public static void main(String[] args) {
-        int argCount = args.length;
-        String filename="";
         String param=""; //[option]
         String paramflag=""; //[option] <outname>, <stage>, etc.
+        CLI(args,param,paramflag);
+
+    }
+
+    public static String readFile(String filename){
+        String FinalString="";
+        try {
+            File stringFile = new File(filename);
+            Scanner in = new Scanner(stringFile);
+            while (in.hasNextLine()) {
+                FinalString += in.nextLine();
+            }
+            in.close();
+        } catch (FileNotFoundException error) {
+            System.out.println("An error occurred: ");
+            error.printStackTrace();
+        }
+        return FinalString;
+    }
+
+    static void CLI(String[] args, String param, String paramflag){
+        String filename="";
+        int argCount = args.length;
         if (argCount==0) {
             CLImenu();
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
             String[] stringArray = input.split(" ");
             for (int i = 0; i < stringArray.length; i++) {
-                if(i==0){
-                    param=stringArray[i];
-                    if (stringArray.length==1){
+                if(i==2){
+                    filename+=stringArray[i];
+                    if (stringArray.length==3){
+                        param="-target";
                         paramflag="scan"; // scan is the default target for now
                     }
                 }
-                if (i==1){
-                    paramflag=stringArray[i].toString();
+                if (i==3){
+                    filename+=stringArray[2];
+                    param=stringArray[i];
+                    if (param == "-target"){
+                        paramflag="scan"; // scan is the default target for now
+                    }
+                }
+                if (i==4){
+                    filename+=stringArray[2];
+                    param=stringArray[3];
+                    paramflag=stringArray[4];
                 }
             }
 
         }
-        else {
+        if (argCount==1){
+            filename+=args[0];
+            param="-target";
+            paramflag="scan"; // scan is the default target for now
+        }
+        if (argCount >=2) {
             for (int i = 0; i < args.length; i++) {
                 if(i==0){
-                    filename=args[i];
+                    filename+=args[i];
                 }
                 if (i==1){
-                    param=args[i].toString();
+                    param=args[i];
                 }
                 if (i==2){
-                    paramflag=args[i].toString();
+                    paramflag=args[i];
                 }
             }
         }
+
+        String Scanstring=readFile(filename);
 
         switch (param){
             case "-o":
+                System.out.println("Output is in "+paramflag);
                 break;
             case "-target":
                 switch (paramflag){
                     case "scan":
                         System.out.println("stage: scanning");
+                        //DScanner(Scanstring);
                         break;
                     case "parse":
                         System.out.println("stage: scanning");
@@ -94,11 +137,12 @@ public class Compiler {
                 System.out.println("Debugging "+paramflag);
                 break;
         }
-
     }
 
+
     static void CLImenu() {
-            System.out.println("Possible options include:\n"+
+            System.out.println("Usage: java Compiler <filename> [option]\n"
+                    +"Possible options include:\n"+
                     "-o <outname>\n" +
                     "-target <stage>\n" +
                     "\t scan" +
