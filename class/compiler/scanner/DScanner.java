ac65b432e016810d4cb945cbd367c7339419ac82
tirso <tirso@ufm.edu>
2020-09-23 22:12:22 -0600

    Refex

--- class/compiler/scanner/DScanner.java
+++ class/compiler/scanner/DScanner.java
@@ -1,29 +1,26 @@
-package compiler.scanner;
+package scanner;
 import java.util.Scanner; // Import the Scanner class to read text files
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.regex.*;
+
 
 public class DScanner {
     public static void main(String[] args) {
-        ReadFile("test.dcf");
+    Regex("main() -> void {\n" +
+            "print(\"Hello world!\\n\");\n" +
+            "\n" +
+            "}");
     }
-    static void ReadFile(String filename) {
-        try {
-            File myObj = new File(filename);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
-            }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
 
-
-        }
+    static String Regex(String program){
+        Pattern pattern = Pattern.compile("Class Program|\\{| }|\\[|]|,|;|=|-|\\+|-=|!|<|>|<=|>=|==|!=|\\+=|\\*|/|&&|\\|\\||%|//|\"|\'|\\\\|\'|\"|\n|\t|\\(|\\)|int|boolran|if|for|return|break|continue|callout|true|false|void|else|0[x|X][\\w|\\d]+|\\d+|[\\w][\\w\\d_]+|[\\W]+ ");
+        Matcher matcher = pattern.matcher(program);
 
     }
+
+
+
 
 
 }
