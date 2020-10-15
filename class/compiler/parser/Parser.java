66210e6c41dd3d659d86292b0f224c7e050b7e83
Ale <alejdelcomp@gmail.com>
2020-10-15 15:15:50 -0600

    Parsing...

--- class/compiler/parser/Parser.java
+++ class/compiler/parser/Parser.java
@@ -3,33 +3,37 @@ package parser;
 
 import scanner.Token;
 
-import java.util.Hashtable;
-import java.util.Stack;
+import java.util.*;
 
 public class Parser {
 
-    static void shift(Stack st, Stack to, Token token, int a) {
+    static void shift(Stack st, Stack to, Token token, int a) { //Mete un token al stack de tokens y un estado al stack de estados
         st.push(a);
         System.out.println("States: " + st);
-        to.push(token);
-        System.out.println("Tokens: " + st);
+        String tokenItem= token.Token;
+        to.push(tokenItem);
+        System.out.println("Tokens: " + to);
     }
 
-    static void goTo(Stack st, int a) {
+    static void goTo(Stack st, int a) { //mete un estado al stack de estados
         st.push(a);
         System.out.println("States: " + st);
     }
 
-    static void reduce(Stack st, Stack to) {
-        System.out.print("pop -> ");
-        Integer a = (Integer) st.pop();
-        System.out.println(a);
+    static void reduce(Stack st, Stack to, int num) { //Se sacan los tokens que se pueden simplificar y se sacan la cantidad de estados que diga
+        for(int i =0; i < num; i++){
+            System.out.print("pop -> ");
+            Integer a = (Integer) st.pop();
+            System.out.println(a);
+        }
         System.out.println("Tokens: " + st);
 
         //Pop from Tokens
-        System.out.print("pop -> ");
-        Integer b = (Integer) to.pop();
-        System.out.println(a);
+        for(int i =0; i < num; i++){
+            System.out.print("pop -> ");
+            Integer b = (Integer) to.pop();
+            System.out.println(b);
+        }
         System.out.println("Tokens: " + to);
 
     }
@@ -40,11 +44,8 @@ public class Parser {
     public static void main(String[] args)  {
         Stack stkTokens = new Stack();
         Stack stkStates = new Stack();
-        int states_counter= 0;
-
         stkStates.push(0);
-        System.out.println("stackTokens: " + stkTokens);
-        System.out.println("stackStates: " + stkStates);
+
 
         //ACTIONS TABLE ARRAYS
         String [] inputSymbols = {"(",")","$","X"};
@@ -56,40 +57,49 @@ public class Parser {
                           {"reduce 2","reduce 2","reduce 2"}};
 
 
-        Token token =new Token("(","terminal","(",0);
-        Token token2 =new Token("(","terminal","(",1);
-        Token token3 =new Token(")","terminal",")",1);
 
+        List<Token> listTokens = new ArrayList<Token>();
+        listTokens.add(new Token("(","terminal"));
+        listTokens.add(new Token("(","terminal"));
+        listTokens.add(new Token(")","terminal"));
+        listTokens.add(new Token(")","terminal"));
 
-        String currentToken=token.getToken;
+        int tokensCounter=0;
+        int listzise=listTokens.size();
 
         for (int i = 0; i < grammar.length; i++){
             for (int j = 0; j < grammar[i].length; j++){
-                if (inputSymbols[j]==currentToken){
-                    System.out.println(i);
-                    System.out.println(j);
+                if (inputSymbols[j]==listTokens.get(tokensCounter).Token){
                     String expression=grammar[i][j];
 
-                    System.out.println(expression);
-                    switch (expression) {
+                    String[] expressionarr = expression.split(" ");
+                    switch (expressionarr[0]) {
                         case "shift":
-                            states_counter=states_counter+1;
-                            shift(stkStates,stkTokens,token,states_counter);
+                            int k=Integer.parseInt(expressionarr[1]);
+                            System.out.println("shift to s"+k);
+                            shift(stkStates,stkTokens,listTokens.get(tokensCounter),k);
+                            i=k-1;
+                            tokensCounter++;
                             break;
                         case "goTo":
-                            states_counter=states_counter+1;
-                            goTo(stkStates,states_counter);
+                            int l=Integer.parseInt(expressionarr[1]);
+                            System.out.println("goto s"+l);
+                            goTo(stkStates,l);
+                            i=l-1;
+                            tokensCounter++;
                             break;
                         case "reduce":
-                            states_counter=states_counter+1;
-                            reduce(stkStates,stkTokens);
+                            int m=Integer.parseInt(expressionarr[1]);
+                            System.out.println("reduce ("+m+")");
+                            reduce(stkStates,stkTokens,m);
+                            i=m-1;
+                            tokensCounter++;
                             break;
-                        case "error":
-                            error();
-                            break;
+                        //case "error":
+                           // error();
+                            //break;
 
                     }
-
                 }
             }
         }
