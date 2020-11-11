c9c34c2a5c032eb071e9507b75d9bf6d3fb7e037
tirso <tirso@ufm.edu>
2020-11-11 16:10:55 -0600

    almost done with tokens

--- class/compiler/scanner/RScanner.java
+++ class/compiler/scanner/RScanner.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Scanner;
 public class RScanner {
     public static boolean symbols(String parameter) {
-        String dictSymbols[] = { "=", "!", "*", "/", "%", "+", "-", "<", "<=", ">=", ">", "==", "!=", "&&", "||" };
+        String dictSymbols[] = {"=", "!", "*", "/", "%", "+", "-", "<", "<=", ">=", ">", "==", "!=", "&&", "||", "+=", "-="};
 
         List<String> listSymbols = Arrays.asList(dictSymbols);
         if (listSymbols.contains(parameter)) {
@@ -17,8 +17,9 @@ public class RScanner {
             return false;
         }
     }
+
     public static boolean brackets(String parameter) {
-        String dictBrackets[] = { "(", ")", "[", "]", "{", "}", ";", "," };
+        String dictBrackets[] = {"(", ")", "[", "]", "{", "}", ";", ","};
         List<String> listBRackets = Arrays.asList(dictBrackets);
         if (listBRackets.contains(parameter)) {
             return true;
@@ -26,8 +27,9 @@ public class RScanner {
             return false;
         }
     }
+
     public static boolean keywords(String parameter) {
-        String dictKeywords[] = { "boolean", "break", "callout", "class", "continue", "else", "false", "for", "if", "int", "return", "true", "void", "main"};
+        String dictKeywords[] = {"boolean", "break", "callout", "class", "continue", "else", "false", "for", "if", "int", "return", "true", "void", "main"};
         List<String> list = Arrays.asList(dictKeywords);
         if (list.contains(parameter)) {
             return true;
@@ -35,8 +37,6 @@ public class RScanner {
             return false;
         }
     }
-
-
 
 
     public static String AutomataVerification(int automata[][], int type, char[] tokens, char[] a) {
@@ -112,20 +112,20 @@ public class RScanner {
         } else {
             char excl[] = token1.toCharArray();
             if (excl[0] == '\'') {
-                nextToken.tipo = AutomataVerification( automaton[3], 1, excl, alpha[2]);
+                nextToken.tipo = AutomataVerification(automaton[3], 1, excl, alpha[2]);
             } else if (excl[0] == '\"') {
                 nextToken.tipo = AutomataVerification(automaton[2], 2, excl, alpha[2]);
             } else if (excl[0] == '0' || excl[0] == '1' || excl[0] == '2' || excl[0] == '3' || excl[0] == '4' || excl[0] == '5' || excl[0] == '6' || excl[0] == '7' || excl[0] == '8' || excl[0] == '9') {
                 nextToken.tipo = AutomataVerification(automaton[0], 3, excl, alpha[0]);
             } else {
-                nextToken.tipo = AutomataVerification( automaton[1], 4, excl, alpha[1]);
+                nextToken.tipo = AutomataVerification(automaton[1], 4, excl, alpha[1]);
             }
         }
         return nextToken.tipo;
     }
 
 
-    public static ArrayList  MainScan (Scanner scan) {
+    public static ArrayList MainScan(Scanner scan) {
         ArrayList<Token> stream = new ArrayList<>();
         ArrayList<String> lexem = new ArrayList<String>();
         ArrayList<String> errorList = new ArrayList<String>();
@@ -135,26 +135,26 @@ public class RScanner {
         String posibleError = "";
 
 
-        char[] hexNum = { '0', 'x', '-', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F' };
-        char[] assig = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_' };
-        char[] strings = { ' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '\\', '"', '\'', 'n', 't' };
-        char[][] alpha = { hexNum, assig, strings };
+        char[] hexNum = {'0', 'x', '-', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F'};
+        char[] assig = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '_'};
+        char[] strings = {' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', '@', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']', '^', '_', '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'o', 'p', 'q', 'r', 's', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', '\\', '"', '\'', 'n', 't'};
+        char[][] alpha = {hexNum, assig, strings};
 
-        int[][] numAut = { { 2, 0, 6, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 3, 4, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-                { 3, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, { 5, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 },
-                { 5, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 }, { 3, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 } };
-        int[][] stringAut = {{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0 },
-                { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 0, 2, 2 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
-        int[][] characOut = {{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0 },
-                { 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 0, 5, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3 },
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },};
-        int[][] identAut = {{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 },
-                { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 } };
-        int[][][] automatas = { numAut, identAut, stringAut, characOut };
+        int[][] numAut = {{2, 0, 6, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {3, 4, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {3, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, {5, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
+                {5, 0, 0, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5}, {3, 0, 0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        int[][] stringAut = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0},
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 4, 0, 2, 2},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        int[][] characOut = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0},
+                {3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 0, 5, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 5, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 3, 3, 3, 3},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},};
+        int[][] identAut = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}};
+        int[][][] automatas = {numAut, identAut, stringAut, characOut};
 
         while (scan.hasNextLine()) {
             String line = scan.nextLine();
@@ -193,7 +193,7 @@ public class RScanner {
 
         for (int i = 0; i < lexem.size(); i++) {
             Token token1 = new Token("", "");
-            AutomataToken( lexem.get(i), token1, automatas, alpha);
+            AutomataToken(lexem.get(i), token1, automatas, alpha);
             boolean lexm = true;
             String pos = "";
             String posx = "";
@@ -206,7 +206,7 @@ public class RScanner {
 
                     if (token2.charAt(token2.length() - 1) == '"') {
                         Token token3 = new Token("", "");
-                        AutomataToken( pos, token3, automatas, alpha);
+                        AutomataToken(pos, token3, automatas, alpha);
 
                         if (!token3.tipo.equals("error")) {
                             token1.tipo = token3.tipo;
@@ -235,7 +235,7 @@ public class RScanner {
                         }
 
                         Token token5 = new Token("", "");
-                        AutomataToken( sb, token5 , automatas, alpha);
+                        AutomataToken(sb, token5, automatas, alpha);
 
                         if (token5.tipo.equals("error")) {
                             stream.add(token4);
@@ -257,6 +257,83 @@ public class RScanner {
                 }
             }
         }
-        return stream;
+        StreamCorrection(stream);
+       return stream;
+    }
+
+    public static ArrayList StreamCorrection(ArrayList<Token> stream) {
+        ArrayList<Token> newTokenStream = new ArrayList<>();
+
+        System.out.println(stream.size());
+
+        for (int i = 0; i < stream.size(); i++) {
+
+            Token toCorrect = new Token(stream.get(i).tipo, stream.get(i).valor);
+
+            if (toCorrect.valor.equals("int") || toCorrect.valor.equals("boolean")) {
+                toCorrect.tipo = "type";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("class")) {
+                toCorrect.tipo = "class";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("program")) {
+                toCorrect.tipo = "program";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("}")) {
+                toCorrect.tipo = "}";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("{")) {
+                toCorrect.tipo = "{";
+                newTokenStream.add(toCorrect);
+            }else if (toCorrect.valor.equals("]")) {
+                toCorrect.tipo = "]";
+                newTokenStream.add(toCorrect);
+            }else if (toCorrect.valor.equals("[")) {
+                toCorrect.tipo = "[";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals(",")) {
+                toCorrect.tipo = ",";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals(")")) {
+                toCorrect.tipo = ")";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("(")) {
+                toCorrect.tipo = "(";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("$")) {
+                toCorrect.tipo = "$";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("if")) {
+                toCorrect.tipo = "if";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("else")) {
+                toCorrect.tipo = "else";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("return")) {
+                toCorrect.tipo = "return";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("break")) {
+                toCorrect.tipo = "break";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("continue")) {
+                toCorrect.tipo = "continue";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("for")) {
+                toCorrect.tipo = "for";
+                newTokenStream.add(toCorrect);
+            } else if (toCorrect.valor.equals("=") || toCorrect.valor.equals("+=") || toCorrect.valor.equals("-=")) {
+                toCorrect.tipo = "assignation";
+                newTokenStream.add(toCorrect);
+            }else{
+                newTokenStream.add(toCorrect);
+            }
+
+
+        }
+        for (int a = 0; a < newTokenStream.size(); a++) {
+            System.out.println(newTokenStream.get(a).tipo + ", " + newTokenStream.get(a).valor);
+        }
+
+        return newTokenStream;
     }
 }
