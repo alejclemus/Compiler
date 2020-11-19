2045867176dd697a1664179c24316d3a5c7d4cfe
tirso <tirso@ufm.edu>
2020-11-18 18:17:27 -0600

    finished tree iteration :)

--- class/compiler/semantic/Semantic.java
+++ class/compiler/semantic/Semantic.java
@@ -1,12 +1,7 @@
 package semantic;
 import java.util.*;
-
-import com.sun.source.tree.Tree;
-
 import scanner.Token;
 
-import java.util.ArrayList;
-import java.util.*;
 
 public class Semantic {
     //building Tree
@@ -24,17 +19,31 @@ public class Semantic {
         return temp;
     }
 public static void TreeCreator(){
-    Node root = newNode(new Token("program_nt","program")); //root
+
+Node root = newNode(new Token("program_nt","program")); //root
     (root.child).add(newNode(new Token("class","class")));//Add child to root
     (root.child).add(newNode(new Token("Program","Program")));
     (root.child).add(newNode(new Token("{","{")));
     (root.child).add(newNode(new Token("field_decl","field_decl")));
-    (root.child.get(3).child).add(newNode(new Token("type","hijo0"))); //Add child to node field_decl
-    (root.child.get(3).child).add(newNode(new Token("id","hijo1")));
-    (root.child.get(3).child).add(newNode(new Token("id","hijo2")));
-    ((root.child.get(3).child).get(0).child).add(newNode(new Token("id", "nivel 4")));
-    ((root.child.get(3).child).get(1).child).add(newNode(new Token("id", "nivel 5")));
-
+    (root.child.get(3).child).add(newNode(new Token("type","int"))); //Add child to node field_decl
+    (root.child.get(3).child).add(newNode(new Token("id","kalshjg89")));
+   /*  Code for testing the tree iteration, it works!
+    Node root = newNode(new Token("program_nt","x")); //root
+    (root.child).add(newNode(new Token("class","0, UNO")));//Add child to root
+    (root.child).add(newNode(new Token("Program","1, SIETE")));
+    (root.child).add(newNode(new Token("{","2, OCHO")));
+    (root.child).add(newNode(new Token("field_decl","3, NUEVE")));
+    (root.child.get(3).child).add(newNode(new Token("type","3.0, DIEZ"))); //Add child to node field_decl
+    (root.child.get(3).child).add(newNode(new Token("id","3.1, DOCE")));
+    (root.child.get(3).child).add(newNode(new Token("id","3.2, CATORCE")));
+    ((root.child.get(3).child).get(0).child).add(newNode(new Token("id", "3.0.1, ONCE")));
+    ((root.child.get(3).child).get(1).child).add(newNode(new Token("id", "3.1.1, TRECE")));
+    (root.child.get(0).child).add(newNode(new Token("type","0.1, DOS")));
+    (root.child.get(0).child).add(newNode(new Token("type","0.2, TRES")));
+    ((root.child.get(0).child).get(1).child).add(newNode(new Token("id", "0.2.1, CUATRO")));
+    ((root.child.get(0).child).get(1).child).add(newNode(new Token("id", "0.2.2 CINCO")));
+    ((root.child.get(0).child).get(1).child).add(newNode(new Token("id", "0.2.3 SEIS")));
+*/
 
 Recorrer(root);
 
